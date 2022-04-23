589f96a9a9fd7f4aa4648e500604c51cf88321a4
baozi <bigbaozibao@qq.com>
2022-04-23 16:43:10 +0800

    初始化

---
+++ src/main/java/org/baozi/CustomProducer.java
@@ -0,0 +1,27 @@
+package org.baozi;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class CustomProducer {
+    public static void main(String[] args) {
+        // 配置
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.21.0.15:9093");
+        // 制定key和value的序列化器
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        // 创建kafka对象
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        // 发送消息
+        for (int i = 0; i < 10; i++)
+            kafkaProducer.send(new ProducerRecord<>("first", "baozi" + i));
+        // 关闭资源
+        kafkaProducer.close();
+    }
+}
---
+++ src/main/java/org/baozi/CustomProducerCallback.java
@@ -0,0 +1,28 @@
+package org.baozi;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class CustomProducerCallback {
+    public static void main(String[] args) {
+        // 配置
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.21.0.15:9093");
+        // 制定key和value的序列化器
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        // 创建kafka对象
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        // 发送消息
+        for (int i = 0; i < 10; i++)
+            kafkaProducer.send(new ProducerRecord<>("first", "baozi" + i), (metadata, exception) -> {
+                if (exception ==null)
+                    System.out.println("主题：" + metadata.topic() + " 分区：" + metadata.partition());
+            });
+        // 关闭资源
+        kafkaProducer.close();
+    }
+}
